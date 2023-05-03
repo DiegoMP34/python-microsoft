791d1eb78101c560899738d015771e389f59f0a2
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-02 23:26:10 -0500

    operadores

--- hello.py
+++ hello.py
@@ -17,3 +17,16 @@ shuttle_landed_on_the_moon = "Apollo 11" #string
 ## Como saber el tip de dato.
 what_type_is = type(shuttle_landed_on_the_moon) ## <class 'str'>
 print(what_type_is)
+
+#operadores - aritmeticos 
+print( 3 + 5) #suma
+print( 9 - 5) #resta
+print( 3 * 5) #multiplicacion
+print( 15 / 5) #division
+
+#operadores de asignacion
+a = 2 #
+a += 2 #incrementa en 2
+a -= 2 #decrementa en 2
+a /= 2 #dividido por 2
+a *= 2 #multiplicado por 2
