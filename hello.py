1ef18b67a3e9ee735c9eda3282a08936246295a8
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-02 23:11:02 -0500

    Variables

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ print('Hello mi mundo')
 # programa - serie de intruciones que cumple un objetivo
 sum = 2 + 4
 print(sum)
+
+# variables - almacenar datos, que usaremos en un punto determinado
+suma = 5 + 9
+product = suma * 3
+print(product)
