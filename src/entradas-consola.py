afaed6e093ac5138add6be5ac4a66f3bfe3f6540
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-06 00:09:32 -0500

    captura de datos del usuario por consola

--- src/entradas-consola.py
+++ src/entradas-consola.py
@@ -8,3 +8,7 @@ print(sys.argv[1]) ## 2023-05-6
 # en consola al momento de ejecutar el programa le pasamos un argumento
 # de esta manera -> python3 entradas-consola.py 2023-05-6
 
+#capturar datos del usuario
+print('Bienvenido al este humilde programa')
+userName = input('Ingresa tu nombre: ')
+print("Saludos " + userName)
