9b7e3194abee645c990d309aceb30ae2f3f995cb
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-10 22:29:42 -0500

    Comento las lineas que me mostrarban una advertencia.

--- src/entorno-virtual/src/app.py
+++ src/entorno-virtual/src/app.py
@@ -1,9 +1,9 @@
 from datetime import *
-from dateutil.relativedelta import *
+#from dateutil.relativedelta import *
 
 now = datetime.now()
 print(now)
 
-now = now + relativedelta(months=1,weeks=1,hours=10)
+#now = now + relativedelta(months=1,weeks=1,hours=10)
 
 print(now)
