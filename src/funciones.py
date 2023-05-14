6c3ba1ddf370b68f87b437981d0358db1d0d1c5d
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-14 12:04:49 -0500

    Argumentos de funciones - palabra clave vartiable=valor - variable *args

--- src/funciones.py
+++ src/funciones.py
@@ -48,3 +48,48 @@ print(dias_a_completar(238855, 75))
 
 #se pueden tener funciones como argumentos 
 print(round(dias_a_completar(238855,75))) #esto puede afectar a la legilibilidad
+
+#argumentos de palabra clave - son argumentos asignados a un valor predeterminado
+## time delta ayuda en la suma de un tipo date.
+from datetime import timedelta, datetime
+
+def tiempo_llegada(hora=51):
+    actual = datetime.now()
+    llegada = actual + timedelta(hours=hora)
+    return llegada.strftime("Llegada: %A %H:%M")
+
+print(tiempo_llegada())
+print(tiempo_llegada(0))
+print(datetime.now())
+
+# combinacion de argumentos y argumentos de palabra clave
+def arrival_time(destination, hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime(f"{destination} Arrival: %A %H:%M")
+
+#arrival_time() #error: al menos un argumento
+print(arrival_time('Moon')) 
+print(arrival_time('Orbit', hours=0.13))
+
+#variable de argumentos - permite pasar cualquier numero de argumentos, incluido 0
+## Para definirlo tenemos colocar * y despues
+## Se usa generalmente args o a para definir este argumento
+def varible_lenght(*args):
+    print(args)
+
+varible_lenght()
+varible_lenght("one","two")
+varible_lenght(None)
+
+def secuencia_tiempo(*args):
+    total_minutos = sum(args)
+    if total_minutos < 60:
+        print(f"Tiempo total de despege {total_minutos} minutos")
+    else:
+        print(f"Tiempo total de despege {total_minutos/60} horas")
+
+secuencia_tiempo()
+secuencia_tiempo(4,14,18)
+# la variable se le denomina catch-all
+
