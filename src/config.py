84917cea2b3dae72281046dd71e4e8155f524875
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-05-16 18:52:05 -0500

    Uso de as para manipular el error arrojado

--- src/config.py
+++ src/config.py
@@ -21,14 +21,30 @@
 # if __name__ == '__main__':
 #     main()
 
-#manejandolo error error
-def main():
-    try:
-        configuration = open('config.txt')
-    except FileNotFoundError:
-        print("Couldn't find the config.txt file!")
-    except IsADirectoryError:
-        print("Found config.txt but it is a directory, couldn't read it")
+#manejandolo error por error
+# def main():
+#     try:
+#         configuration = open('config.txt')
+#     except FileNotFoundError:
+#         print("Couldn't find the config.txt file!")
+#     except IsADirectoryError:
+#         print("Found config.txt but it is a directory, couldn't read it")
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+#uso de as, para tener al error com variable
+try:
+    open("mars.jpg")
+except FileNotFoundError as err:
+    print("got a problem trying to read the file:", err)
+
+#Para excepciones mas genericas, se puede ingresar al err
+## por ejemplo OSError que abarca tanto FileNotFoundError 
+try:
+    open('mars.jpg')
+except OSError as err :
+    if(err.errno == 2):
+        print("Couldn't find the config.txt file!")
+    elif err.errno == 13:
+        print("Found config.txt but couldn't read it")
